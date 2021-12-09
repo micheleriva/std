7165cd63a7831a93dd77e5663d4db6e17f805198
Michele Riva <master@micheleriva.it>
2021-12-09 09:18:47 +0100

    refactor: removed ts-jest in favor of swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
 };
